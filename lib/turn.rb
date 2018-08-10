f21d6bcae9a0ecc82097b0de6ba66d4ac2e48e6c
Tak H Kim <takhyun818@gmail.com>
2018-08-10 20:00:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,6 +29,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
+    display_board(board)
   else
     puts "That's not a valid move."
     turn(board)
