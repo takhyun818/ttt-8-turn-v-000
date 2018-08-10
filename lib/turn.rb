71470c605993e8197e16de520555b1e6d11b0ad0
Tak H Kim <takhyun818@gmail.com>
2018-08-10 19:49:50 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,3 +10,11 @@ def input_to_index(user_input)
   user_input.to_i - 1
 
 end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+end
